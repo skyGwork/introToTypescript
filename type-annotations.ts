e03501404b21ffd309d09db5113da93ca4bc6d2e
skyGwork <skyGworkk@gmail.com>
2022-09-13 20:02:49 +0530

    update

---
+++ type-annotations.ts
@@ -0,0 +1,58 @@
+let apples = 5;
+let speed: string = 'fast';
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+// built in objects
+let now: Date = new Date();
+
+// Array
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, true, false];
+
+// Object literal
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: 'John',
+  age: 25,
+};
+
+// another object
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
+
+// Function
+//annotation
+let greeting: (name: string) => string;
+greeting = function (name: string) {
+  return `Hi ${name}`;
+};
+
+// combined at one function with annotation
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i); // for void
+};
+const logNumber1: (i: number) => string = (i: number) => {
+  return 'hello all'; // for string
+};
+const logNumber2: (i: number) => boolean = (i: number) => {
+  return true; // boolean
+};
+
+let greetingAll: (name: string, age: number) => string = (name: string) => {
+  return `Hi ${name}`;
+};
+
+// Classes
+class Car {}
+let car: Car = new Car();
+
