4e20acc5137c7f2fbb03746b947f4c83fe74e223
skyGwork <skyGworkk@gmail.com>
2022-09-15 19:28:33 +0530

    update

---
+++ src/types/class/use.ts
@@ -0,0 +1,22 @@
+// !JavaScript constructor functions
+class Person {
+  firstName: string;
+  lastName;
+  age: number;
+
+  constructor(fstName: string, lstName?: string, age: number = 10) {
+    this.firstName = fstName;
+    this.lastName = lstName;
+    this.age = age;
+  }
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
+let selOne = new Person('SEL', 'learning');
+console.log(selOne.age);
+console.log(selOne.getFullName());
+
+let selTwo = new Person('skyinnk', 'Next learning', 30);
+console.log(selTwo.age);
+console.log(selTwo.getFullName());
